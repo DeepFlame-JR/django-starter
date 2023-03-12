da534bded5b56f9157d632ca8561bc4552ebd2a0
JunRyeol <wnsfuf0121@naver.com>
2023-03-12 13:20:32 +0900

    Add Templates

--- projects/first-django/first/views.py
+++ projects/first-django/first/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    now = datetime.now
+    context = {
+        'current_date': now
+    } # 삽입하길 원하는변수
+    return render(request, 'index.html', context)
 
 
-def select(requet):
-    message = "수 하나를 입력해주세요."
-    return HttpResponse(message)
+def select(request):
+    context = {'number' : 3}
+    return render(request, 'select.html', context)
 
 
 def result(request):
-    message = "추첨 결과입니다."
-    return HttpResponse(message)
+    context = {'numbers': [1,2,3,4,5,6]}
+    return render(request, 'result.html', context)
