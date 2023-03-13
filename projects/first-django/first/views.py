4f6d799837ba4282c6055356448c308970d365b7
JunRyeol <wnsfuf0121@naver.com>
2023-03-13 19:32:41 +0900

    Create random lotto number webapp

--- projects/first-django/first/views.py
+++ projects/first-django/first/views.py
@@ -3,20 +3,39 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 
+import random 
+
 # Create your views here.
 def index(request):
     now = datetime.now
     context = {
         'current_date': now
     } # 삽입하길 원하는변수
-    return render(request, 'index.html', context)
+    return render(request, 'first/index.html', context)
 
 
 def select(request):
-    context = {'number' : 3}
-    return render(request, 'select.html', context)
+    context = {}
+    return render(request, 'first/select.html', context)
 
 
 def result(request):
-    context = {'numbers': [1,2,3,4,5,6]}
-    return render(request, 'result.html', context)
+    chosen = int(request.GET['number'])
+
+    results = []
+    if chosen >= 1 and chosen <= 45:
+        results.append(chosen)
+    
+    box = []
+    for i in range(0, 45):
+        if chosen != i+1:
+            box.append(i+1)
+            
+    random.shuffle(box)
+    results.extend(box[:5])
+    
+    context = {
+        'numbers': results
+    }
+    
+    return render(request, 'first/result.html', context)
