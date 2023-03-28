115644b39ade062880dd92f7b0f4fb64a4389c38
JunRyeol <wnsfuf0121@naver.com>
2023-03-28 18:29:10 +0900

    [Django] 리스트 화면 구성 + page

--- projects/first-django/third/views.py
+++ projects/first-django/third/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from third.models import Restaurant
+from django.core.paginator import Paginator
 
 # Create your views here.
 def list(request):
-    context = {}
-    return render(request, 'thrid/list.html', context)
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)
+    
+    # request에 페이지 이름이 부여됨 /third/list?page=1
+    page = request.GET.get('page')
+    # 해당 페이지의 아이템만 필터링해서 가져온다
+    items = paginator.get_page(page)
+    
+    context = {
+        'restaurants': items
+    }
+    return render(request, 'third/list.html', context)
