db5528411f50714abfd4ab3177a422b83689a061
JunRyeol <wnsfuf0121@naver.com>
2023-04-02 12:33:26 +0900

    [Django] add review view

--- projects/first-django/third/models.py
+++ projects/first-django/third/models.py
@@ -7,3 +7,12 @@ class Restaurant(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    point = models.IntegerField()
+    comment = models.CharField(max_length=500)
+    
+    Restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)  # 만약 Restaurant가 삭제된다면 어떻게 처리할 것인가
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
